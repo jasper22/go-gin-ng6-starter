3b8dad6f3f184fe98c0906487443adfd36429309
jgoerner <joshua.goerner@gmail.com>
2018-08-19 17:50:34 +0200

    Change Viper as single access point to config vars

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,15 +1,16 @@
 package server
 
-import "go-gin-ng6-starter/backend/utils"
+import (
+    "go-gin-ng6-starter/backend/config"
+)
 
 func Init() {
     // setup router
     r := SetupRouter()
 
     // get port from env or use default 8080
-    port := utils.GetEnv("PORT", "8080")
-
-    // TODO: add ENV here (prod, dev, test) and setup router, server, db based on config
+    cfg := config.GetConfig()
+    port := cfg.GetString("gin.port")
 
     // run http server
     r.Run(":" + port)
